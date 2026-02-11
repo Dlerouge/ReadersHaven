fc814d1a9f26f9193a7733d43eb65c84f988c162
De'ovyun LeRouge <lerouge966@gmail.com>
2026-02-11 16:37:53 -0500

    Adding files

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// IMPORTANT: Restrict this API key in Google Cloud Console
+// Application restriction: HTTP referrers
+// Add:
+//   https://YOURUSERNAME.github.io/*
+//   http://localhost:*
+// API restriction: Books API
+
+window.GOOGLE_BOOKS_API_KEY = "PASTE_YOUR_RESTRICTED_KEY_HERE";
